5c79d59ba9aa9e3cbae881ed695a57776ddd1588
marigotten <hum.our.mar@gmail.com>
2022-02-23 15:24:10 +0900

    add API info

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://marigotten.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': 'd68f8f45652e433dad584abc2e0e3b7f7573' }
+      }
+    )
+    return data
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'IndexPage'
+  async asyncData() {
+    const { data } = await axios.get(
+      'https://marigotten.microcms.io/api/v1/blog',
+      {
+        headers: { 'X-MICROCMS-API-KEY': 'd68f8f45652e433dad584abc2e0e3b7f7573' }
+      }
+    )
+    return data
+  }
 }
 </script>
